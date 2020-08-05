40f9ebbf568243ec913ceb03658649cd2014ef79
Nico <n.gaget@gmail.com>
2020-08-05 12:30:24 +0200

    initialisation symfony / composer / yarn / bootstrap / jquery

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,7 +4,21 @@
 namespace App\Controller;
 
 
-class HomeController
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
 {
+    /**
+     * @return Response
+     * @Route ("/", name="index")
+     */
+    public function index()
+    {
+        return $this->render('index.html.twig', [
+            'pageTitle' => 'Home Portfolio'
+        ]);
+    }
 
 }
