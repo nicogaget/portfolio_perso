229e5c69952e59b3aa710c317d8f43c4722b45b7
Nico <n.gaget@gmail.com>
2020-08-28 17:15:07 +0200

    HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 
 
 use App\Entity\Contact;
+use App\Entity\Projet;
 use App\Form\ContactType;
 use App\Notification\ContactNotification;
 use App\Repository\ProfilRepository;
+use App\Repository\ProjetRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,12 +21,14 @@ class HomeController extends AbstractController
      * @param ProfilRepository $profilRepo
      * @param Request $request
      * @param ContactNotification $notification
+     * @param Projet $projet
      * @return Response
      * @Route ("/", name="index")
      */
-    public function index(ProfilRepository $profilRepo, Request $request, ContactNotification $notification)
+    public function index(ProfilRepository $profilRepo, Request $request, ContactNotification $notification, ProjetRepository $projetRepo)
     {
         $profil = $profilRepo->findOneBy([]);
+        $projets = $projetRepo->findAll();
         $contact = new Contact();
         $form = $this->createForm(ContactType::class,$contact);
         $form->handleRequest($request);
@@ -33,7 +37,7 @@ class HomeController extends AbstractController
             $notification->notify($contact);
             $this->addFlash('success', "Votre message a bien été envoyé");
 
-//            return $this->redirectToRoute('index');
+            return $this->redirectToRoute('index');
 
         }
 
@@ -41,7 +45,8 @@ class HomeController extends AbstractController
         return $this->render('index.html.twig', [
             'pageTitle' =>  'Home Portfolio',
             'profil'    =>  $profil,
-            'form'      =>  $form->createView()
+            'form'      =>  $form->createView(),
+            'projets'   => $projets
         ]);
     }
 
