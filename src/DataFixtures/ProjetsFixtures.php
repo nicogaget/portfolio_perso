39c33dcbe5679eef530b607a48d4c723e5d8c244
Nico <n.gaget@gmail.com>
2020-08-28 17:17:29 +0200

    fixtures

--- src/DataFixtures/ProjetsFixtures.php
+++ src/DataFixtures/ProjetsFixtures.php
@@ -22,11 +22,11 @@ class ProjetsFixtures extends Fixture
         $projet->setPitch("“Des échanges sans argent pour des liens plus humains”");
         $projet->setDescription1("Offrir une plateforme de troc entre particuliers, sans enjeu monétaire, dans un esprit de bienveillance.");
         $projet->setDescription2("Site fictif développer dans le cadre de la formation Wild Code School en équipe de 4 personnes sur 4 semaines. Utilisation des méthodes Agiles et SCRUM");
-        $projet->
-        $projet->addLanguage($this->getReference("PHP"));
-        $projet->addLanguage($this->getReference("JavaScript"));
-        $projet->addLanguage($this->getReference("CSS"));
-        $projet->addLanguage($this->getReference("HTML"));
+        $projet->addLanguage($this->getReference("linux"));
+        $projet->addLanguage($this->getReference("php"));
+        $projet->addLanguage($this->getReference("js"));
+        $projet->addLanguage($this->getReference("css"));
+        $projet->addLanguage($this->getReference("html"));
         $projet->addLanguage($this->getReference("github"));
         $projet->setUpdatedAt(new \DateTime('now'));
         $projet->setSlug($slug->generate($projet->getName()));
@@ -36,16 +36,17 @@ class ProjetsFixtures extends Fixture
         $projet = new Projet();
 
         $projet->setName("Rocket School");
-        $projet->setPitch("Outil destiné aux candidats de l'école Rocket School qui propose desformations en Business Development et Webmarketing. Cet outil leut permet de s'informer sur les différents métiers accessibles après une formation.");
-        $projet->setDescription1("MOOC avec une vidéo d'information sur les formations proposées par la Rocker School ainsi qu'un quizz en lien avec la vidéo.");
+        $projet->setPitch("MOOC avec une vidéo d'information sur les formations proposées par la Rocker School ainsi qu'un quizz en lien avec la vidéo.");
+        $projet->setDescription1("Outil destiné aux candidats de l'école Rocket School qui propose desformations en Business Development et Webmarketing. Cet outil leut permet de s'informer sur les différents métiers accessibles après une formation.");
         $projet->setDescription2("Développement d'une interface 'candidat' pour un acces simple à la vidéo, au quizz, à une FAQ, à son profil candidat ainsi qu'a un guide destiné à la suite de son parcours candidat.
 Développement également de l'interface 'admin', avec un CRUD complet pour modifier et mettre à jour la totalité du contenu proposé.");
         $projet->addLanguage($this->getReference("linux"));
-        $projet->addLanguage($this->getReference("PHP"));
-        $projet->addLanguage($this->getReference("Symfony"));
-        $projet->addLanguage($this->getReference("JavaScript"));
-        $projet->addLanguage($this->getReference("CSS"));
-        $projet->addLanguage($this->getReference("HTML"));
+        $projet->addLanguage($this->getReference("php"));
+        $projet->addLanguage($this->getReference("symfony"));
+        $projet->addLanguage($this->getReference("js"));
+        $projet->addLanguage($this->getReference("css"));
+        $projet->addLanguage($this->getReference("sass"));
+        $projet->addLanguage($this->getReference("html"));
         $projet->addLanguage($this->getReference("github"));
         $projet->setUpdatedAt(new \DateTime('now'));
         $projet->setSlug($slug->generate($projet->getName()));
